ae165cd6e5463f77c49a84ffaf1ed0553e67b608
Chitica <liseth_gdl_86@hotmail.com>
2018-07-09 18:53:03 -0500

    mongo

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGOLAB_YELLOW_URI || "mongodb://localhost/week18Populater";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 var PORT = 4000;
 
 // Initialize Express
